41971f7d4c75fd1869b026f5acfbef7f2b7e5c50
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-12-25 14:21:39 +0545

    animation added in the nav bar

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function myFunctionChange(){
         menuBtn.className = "nav-menu";
     }
 }
+
+// Add shadow on navigation bar while scrolling
+window.onscroll = function(){headerShadow()};
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    }else{
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
