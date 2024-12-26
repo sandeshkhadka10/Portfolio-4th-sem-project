72eb7d1394f42538fefeb6c12090df771fa03fba
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-12-26 10:13:30 +0545

    html, css and js code completed but problem in nav bar

--- app.js
+++ app.js
@@ -83,17 +83,17 @@ srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
 
 // Change active link
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll('section[id]')
 function scrollActive(){
     const scrollY = window.scrollY;
     sections.forEach(current=>{
         const sectionHeight = current.offsetTop - 50,
-        sectionId = current.getAttribute('id');
+        sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.add('active-link');
+            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.add('active-link')
         }else{
-            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.remove('active-link');
+            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.remove('active-link')
         }
     })
 }
-window.addEventListener('scroll',scrollActive);
+window.addEventListener('scroll',scrollActive)
