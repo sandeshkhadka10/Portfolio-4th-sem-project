055d8cfd02c018e9c16b113086053b3bc6986735
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-12-26 09:41:14 +0545

    added scroll animation in active link

--- app.js
+++ app.js
@@ -82,3 +82,18 @@ srRight.reveal('.skills-box',{delay: 100})
 // Form Box
 srRight.reveal('.form-control',{delay: 100})
 
+// Change active link
+const sections = document.querySelectorAll('section[id]');
+function scrollActive(){
+    const scrollY = window.scrollY;
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetTop - 50,
+        sectionId = current.getAttribute('id');
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId +']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive);
